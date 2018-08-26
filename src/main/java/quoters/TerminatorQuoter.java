c28a4255a450842b532ad9321746f92262fb5f17
Igor Volgin <igor_volgin@epam.com>
2018-08-26 20:01:15 +0300

    Profiling RetentionPolicy fix

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -24,6 +24,8 @@ public class TerminatorQuoter implements Quoter {
         this.message = message;
     }
 
+    @Override
+    @PostConstruct
     public void sayQuote() {
         for (int i = 0; i < repeat; i++) {
             System.out.println("message = " + message);
