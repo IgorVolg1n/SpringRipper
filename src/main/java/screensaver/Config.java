e6809964df4483b2aa133c8f531d1b1c8b613635
Igor Volgin <igor_volgin@epam.com>
2018-08-26 21:57:52 +0300

    periodical screensaver

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -8,19 +8,13 @@ import java.util.Random;
 @Configuration
 @ComponentScan(basePackages = "screensaver")
 public class Config {
+
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         while (true) {
             context.getBean(ColorFrame.class).showOnRandomPlace();
             Thread.sleep(50);
         }
-    }
-
-    @Bean
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public Color color() {
-        Random random = new Random();
-        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
     }
 
     @Bean
@@ -31,5 +25,12 @@ public class Config {
                 return color();
             }
         };
+    }
+
+    @Bean
+    @Scope(value = "periodical")
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
     }
 }
